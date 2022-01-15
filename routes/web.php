42ea1d17776aa2a2da91f84d54b82ed01650d07b
lecbit <artm-marych@rambler.ru>
2022-01-15 12:32:48 +0200

    ~3~ make_first_controller

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/myPage', function(){
-    return "This is myPage";
-});
+Route::get('/myPage', "MyPlaceController@index");
