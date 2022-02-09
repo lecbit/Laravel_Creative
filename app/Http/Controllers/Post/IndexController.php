89926d85a8ed9484f4690ff8b066ddac81d5df5b
lecbit <artm-marych@rambler.ru>
2022-02-09 18:55:23 +0200

    ~30~ Filter files setting

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\PostFilter;
+use App\Http\Requests\FilterRequest;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\PostTag;
@@ -12,9 +14,37 @@ use Illuminate\Http\Request;
 
 class IndexController extends BaseController
 {
-    public function __invoke()
+    public function __invoke(FilterRequest $request)
     {
-        $posts = Post::paginate(5);
+        $data = $request->validated();
+
+        $filter = app()->make(PostFilter::class, ['queryParams' => array_filter($data)]);
+        $posts = Post::filter($filter)->paginate(1);
+
+        //2
+        // $data = $request->validated();
+        // $query = Post::query();
+
+        // if (isset($data['category_id'])) {
+        //     $query->where('category_id', $data['category_id']);
+        // }
+
+        // if(isset($data['title']))
+        // {
+        //     $query->where('title', 'like', "%{$data['title']}%");
+        // }
+
+        // $posts = $query->get();
+        // dd($posts);
+        ///
+
+        //1
+        // $posts = Post::where('is_published', 1)
+        // ->where('category_id', $data['category_id'])
+        // ->get();
+        ///
+
+
         return view('posts.index', compact('posts'));
     }
 }
